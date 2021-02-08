6f94d8d8e26044dffff683e41cf5e5d63b2e91b3
Kelly Gemmill <kgemmill@ucla.edu>
2021-02-07 16:38:19 -0800

    Updating footer to point to new sudokusolver repository. Updating App.js to point to kellygemmill.com version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,14 +15,10 @@ const buttonStyle = {
 const Footer = () => {
     return (
         <div style={style}>
-            <p style={{ marginBottom: "0", fontWeight: "bold" }}>Source code available on Github:</p>
-            <Button style={buttonStyle} variant="outline-dark" href="https://github.com/kellygemmill/SudokuSolverWeb-Frontend"
-                target="_blank" rel="noopener noreferrer">Frontend (React)</Button> {'  '}
-            <Button style={buttonStyle} variant="outline-dark" href="https://github.com/kellygemmill/SudokuSolverWeb-Backend"
-                target="_blank" rel="noopener noreferrer">Backend (Java)</Button>
-
-            <p style={{ marginTop: "20px" }}>&copy; {new Date().getFullYear()} {' '}
-                <a href="https://www.linkedin.com/in/kelly-gemmill" target="_blank" rel="noopener noreferrer">Kelly Gemmill</a></p>
+            <Button style={buttonStyle} variant="outline-dark" href="https://github.com/kellygemmill/SudokuSolver"
+                target="_blank" rel="noopener noreferrer">View on Github</Button>
+            <p style={{ marginTop: "5px" }}>&copy; {new Date().getFullYear()} {' '}
+                <a href="http://www.kellygemmill.com" target="_blank" rel="noopener noreferrer">Kelly Gemmill</a></p>
         </div>
     )
 }
